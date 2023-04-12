d1f638f03446e78eb423135d848fc9acee47b084
Thaygobrd <tbrduarte@gmail.com>
2023-04-12 10:19:01 -0300

    feat: Global Styles, BaseTypography and BaseButton

---
+++ src/components/typography.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+interface iBaseTypographyProps {
+  className?: string;
+  tag?:
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "h7"
+    | "p"
+    | "span"
+    | "small"
+    | "label"
+    | "caption";
+  children?: React.ReactNode;
+  fontSize?: string | number;
+  opacity?: number;
+  fontWeight?: 700 | 600 | 500 | 400;
+  color?: string;
+}
+
+const BaseTypography = ({ children, className, tag }: iBaseTypographyProps) => {
+  return (
+    <>
+      {!tag && <p className={className}>{children}</p>}
+      {tag === "h1" && <h1 className={className}>{children}</h1>}
+      {tag === "h2" && <h2 className={className}>{children}</h2>}
+      {tag === "h3" && <h3 className={className}>{children}</h3>}
+      {tag === "h4" && <h4 className={className}>{children}</h4>}
+      {tag === "h5" && <h5 className={className}>{children}</h5>}
+      {tag === "h6" && <h6 className={className}>{children}</h6>}
+      {tag === "h7" && <h6 className={className}>{children}</h6>}
+      {tag === "p" && <p className={className}>{children}</p>}
+      {tag === "span" && <span className={className}>{children}</span>}
+      {tag === "small" && <small className={className}>{children}</small>}
+      {tag === "label" && <label className={className}>{children}</label>}
+      {tag === "caption" && <caption className={className}>{children}</caption>}
+    </>
+  );
+};
+
+export default BaseTypography;
---
+++ src/styles/buttons.ts
@@ -0,0 +1,175 @@
+import styled, { css } from "styled-components";
+import BaseButton from "../components/buttons";
+
+export const StyledButton = styled(BaseButton)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 28px;
+  gap: 10px;
+  border: 1.5px solid transparent;
+  border-radius: 4px;
+  font-weight: 600;
+  font-size: 16px;
+  cursor: pointer;
+
+  ${({ buttonStyle }) => {
+    switch (buttonStyle) {
+      case "grey1":
+        return css`
+          background-color: var(--grey-0);
+          color: var(--white-fixed);
+
+          &&:hover {
+            background-color: var(--grey-1);
+            border-color: var(--grey-1);
+          }
+        `;
+      case "negative":
+        return css`
+          background-color: var(--grey-6);
+          color: var(--grey-2);
+
+          &&:hover {
+            background-color: var(--grey-5);
+            border-color: var(--grey-5);
+          }
+        `;
+      case "disabled":
+        return css`
+          background-color: var(--grey-5);
+          color: var(--white-fixed);
+          cursor: not-allowed;
+        `;
+      case "brand1":
+        return css`
+          background-color: var(--brand-1);
+          color: var(--white-fixed);
+
+          &&:hover {
+            background-color: var(--brand-2);
+            border-color: var(--brand-2);
+          }
+        `;
+      case "brandOpacity":
+        return css`
+          background-color: var(--brand-4);
+          color: var(--brand-1);
+        `;
+      case "light":
+        return css`
+          background-color: var(--grey-10);
+          color: var(--grey-1);
+        `;
+      case "outlineLight":
+        return css`
+          background-color: transparent;
+          color: var(--grey-10);
+
+          &&:hover {
+            background-color: var(--grey-10);
+            border-color: var(--grey-10);
+            color: var(--grey-1);
+          }
+        `;
+      case "outline1":
+        return css`
+          background-color: transparent;
+          color: var(--grey-1);
+          border-color: var(--grey-1);
+
+          &&:hover {
+            background-color: var(--grey-1);
+            border-color: var(--grey-1);
+            color: var(--white-fixed);
+          }
+        `;
+      case "outline2":
+        return css`
+          background-color: transparent;
+          color: var(--grey-2);
+          border-color: var(--grey-4);
+
+          &&:hover {
+            background-color: var(--grey-1);
+            border-color: var(--grey-1);
+            color: var(--white-fixed);
+          }
+        `;
+      case "outlineBrand":
+        return css`
+          background-color: transparent;
+          color: var(--brand-1);
+          border-color: var(--brand-1);
+
+          &&:hover {
+            background-color: var(--brand-4);
+          }
+        `;
+      case "alert":
+        return css`
+          background-color: var(--alert-3);
+          color: var(--alert-1);
+          border-color: var(--alert-3);
+
+          &&:hover {
+            background-color: var(--alert-2);
+            border-color: var(--alert-2);
+          }
+        `;
+      case "success":
+        return css`
+          background-color: var(--success-3);
+          color: var(--success-1);
+          border-color: var(--success-2);
+
+          &&:hover {
+            background-color: var(--success-2);
+            border-color: var(--success-2);
+          }
+        `;
+      case "brandDisabled":
+        return css`
+          background-color: var(--brand-3);
+          color: var(--brand-4);
+          cursor: not-allowed;
+        `;
+      default:
+        return css`
+          background-color: var(--brand-1);
+          color: var(--white-fixed);
+
+          &&:hover {
+            background-color: var(--brand-2);
+            border-color: var(--brand-2);
+          }
+        `;
+    }
+  }}
+
+  ${({ buttonSize }) => {
+    switch (buttonSize) {
+      case "big":
+        return css`
+          width: 279px;
+          height: 48px;
+        `;
+      case "default":
+        return css`
+          width: 146px;
+          height: 48px;
+        `;
+      case "small":
+        return css`
+          width: 119px;
+          height: 38px;
+        `;
+      default:
+        return css`
+          width: 146px;
+          height: 48px;
+        `;
+    }
+  }}
+`;
