f42fc02dbf0b59665807850c93f864767dcbe5d5
Josiel Borges <josielborges555@gmail.com>
2023-04-13 14:23:01 -0300

    feat: base interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,28 @@
+export interface iUser {
+  id: string;
+  name: string;
+  email: string;
+  profile_image: string | null;
+  password: string;
+  cpf: string;
+  phone: string;
+  birthDate: Date;
+  isSeller: boolean;
+  description: string;
+}
+
+export interface iAdvertisement {
+  id: string;
+  brand: string;
+  model: string;
+  year: number;
+  fuel: number;
+  mileage: number;
+  color: string;
+  fipePrice: number;
+  price: number;
+  descripition: string;
+  is_active: boolean;
+  seller: iUser;
+  images: string[];
+}
